07b7b2b400adac9245186690eb344cbdd4408fe4
Petru Rares Sincraian <psincraian@outlook.com>
2018-06-05 21:08:42 +0200

    feat: use shorter badge (#5)

---
+++ tests/unit/application/test_query.py
@@ -0,0 +1,34 @@
+import pytest
+
+from pepy.application.query import DownloadsNumberFormatter
+from pepy.domain.model import Downloads
+
+
+@pytest.fixture
+def downloads_formatter():
+    return DownloadsNumberFormatter()
+
+
+def test_downloads_format_less_than_thousands(downloads_formatter: DownloadsNumberFormatter):
+    downloads = Downloads(121)
+    assert '121' == downloads_formatter.format(downloads)
+
+
+def test_downloads_format_thousands(downloads_formatter: DownloadsNumberFormatter):
+    downloads = Downloads(12_132)
+    assert '12k' == downloads_formatter.format(downloads)
+
+
+def test_downloads_format_million(downloads_formatter: DownloadsNumberFormatter):
+    downloads = Downloads(412_132_492)
+    assert '412M' == downloads_formatter.format(downloads)
+
+
+def test_downloads_format_billion(downloads_formatter: DownloadsNumberFormatter):
+    downloads = Downloads(9_132_919_492)
+    assert '9G' == downloads_formatter.format(downloads)
+
+
+def test_downloads_format_trillion(downloads_formatter: DownloadsNumberFormatter):
+    downloads = Downloads(11_132_919_492_000)
+    assert '11T' == downloads_formatter.format(downloads)

880d9ad66baca1ffcf2f92d9dc90ada417e4db86
Petru Rares Sincraian <psincraian@outlook.com>
2019-07-10 21:40:25 +0200

    feat: remove robots.txt

--- pepy/infrastructure/web/__init__.py
+++ pepy/infrastructure/web/__init__.py
@@ -1,9 +1,6 @@
 import traceback
-from datetime import datetime, timedelta
-from flask import Flask, request, send_from_directory, Response
+from flask import Flask, request, Response
 
-from pepy.application.command import UpdateDownloads
-from pepy.domain.model import ProjectName, Password
 from pepy.infrastructure import container
 from pepy.infrastructure.api import api
 
@@ -28,27 +25,6 @@ def badge_month_action(project_name):
 def badge_week_action(project_name):
     badge = container.badge_query.generate_last_7_days_badge(project_name)
     return Response(badge.image, mimetype="image/svg+xml", headers={"Cache-Control": "max-age=86400"})
-
-
-@app.route("/robots.txt")
-def robots():
-    return send_from_directory(app.static_folder, request.path[1:])
-
-
-@app.route("/task/update_downloads", methods=["POST"])
-def update_downloads():
-    raw_date = request.args.get("date")
-    try:
-        if raw_date is not None:
-            date = datetime.strptime(raw_date, "%Y-%m-%d")
-        else:
-            date = datetime.now() - timedelta(days=1)
-    except ValueError:
-        return Response("Date format should be YYYY-mm-dd", 400)
-
-    password = request.args.get("password", "")
-    container.command_bus.publish(UpdateDownloads(date.date(), Password(password)))
-    return "Updated :-)"
 
 
 @app.errorhandler(Exception)

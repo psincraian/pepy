f0f0a71ee19eaf33d46f8ac8c95db40d83998abc
Daghan Altas <daghan@gmail.com>
2020-01-02 14:40:03 -0800

    replacing json.dumps with flask jsonify helper wrapper

--- pepy/infrastructure/api/__init__.py
+++ pepy/infrastructure/api/__init__.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from pepy.infrastructure import container
 from pepy.infrastructure.api._transformer import transform_project, transform_project_item
 
@@ -10,10 +10,10 @@ api = Blueprint("api", __name__)
 @api.route("/projects/<project_name>", methods=["GET"])
 def project_action(project_name):
     project = container.project_provider.find(project_name)
-    return json.dumps(transform_project(project))
+    return jsonify(transform_project(project))
 
 
 @api.route("/projects", methods=["GET"])
 def project_find():
     projects = container.project_provider.for_home()
-    return json.dumps([transform_project_item(p) for p in projects])
+    return jsonify([transform_project_item(p) for p in projects])

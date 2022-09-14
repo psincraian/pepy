1e1b63bc39d150192f1c319a098da277b136e89b
Petru Rares Sincraian <hi@petru.tech>
2022-09-14 07:06:28 +0200

    feat: configure terraform cloud

--- infrastructure/terraform/versions.tf
+++ infrastructure/terraform/versions.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "pepy"
+    workspaces {
+      tags = ["pepy"]
+    }
+  }
+
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"

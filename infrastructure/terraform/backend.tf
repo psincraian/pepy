e82c986ea9c6e5f354a31c5858987539a2710aa7
Petru Rares Sincraian <hi@petru.tech>
2022-09-18 18:03:06 +0200

    feat: remove local-exec from terraform

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -4,11 +4,4 @@ resource "digitalocean_droplet" "backend" {
   region   = var.region
   size     = "s-1vcpu-512mb-10gb"
   ssh_keys = [data.digitalocean_ssh_key.terraform.id]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      "sudo apt-get install ansible -y"
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.ssh_private_key} provision-web.yml"
-    EOT
-  }
 }
--- infrastructure/terraform/database.tf
+++ infrastructure/terraform/database.tf
@@ -5,13 +5,6 @@ resource "digitalocean_droplet" "database" {
   size     = "s-1vcpu-1gb"
   backups  = true
   ssh_keys = [data.digitalocean_ssh_key.terraform.id]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      "sudo apt-get install ansible -y"
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.ssh_private_key} provision-db.yml"
-    EOT
-  }
 }
 
 resource "digitalocean_volume" "db_volume" {

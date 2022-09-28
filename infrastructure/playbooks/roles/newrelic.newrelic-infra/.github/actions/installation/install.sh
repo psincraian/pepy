1400d293ee9aa43bb34440faea3856b3ff5a3e1a
Petru Rares Sincraian <hi@petru.tech>
2022-09-28 22:17:32 +0200

    feat: add swap and newrelic monitoring to ansible

--- infrastructure/playbooks/roles/newrelic.newrelic-infra/.github/actions/installation/install.sh
+++
@@ -1,9 +0,0 @@
-sudo pipx uninstall ansible-core
-sudo pip3 install 'rich>=10.0.0,<11.0.0'
-sudo pip3 install ansible-lint[community]==5.3.2
-sudo pip3 install molecule[docker]==3.6.0
-
-
-ansible --version
-ansible-lint --version
-molecule --version
